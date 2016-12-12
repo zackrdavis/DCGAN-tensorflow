9b343758e5842e5f9ec2bbf0ea2214138f5013e9
zack <zackrdavis@gmail.com>
2016-12-11 19:45:26 -0500

    squash images instead of cropping

--- utils.py
+++ utils.py
@@ -48,8 +48,9 @@ def center_crop(x, crop_h, crop_w=None, resize_w=64):
     h, w = x.shape[:2]
     j = int(round((h - crop_h)/2.))
     i = int(round((w - crop_w)/2.))
-    return scipy.misc.imresize(x[j:j+crop_h, i:i+crop_w],
-                               [resize_w, resize_w])
+    #return scipy.misc.imresize(x[j:j+crop_h, i:i+crop_w], [resize_w, resize_w])
+    # squash image to fit instead of cropping
+    return scipy.misc.imresize(x, [resize_w, resize_w])
 
 def transform(image, npx=64, is_crop=True, resize_w=64):
     # npx : # of pixels width/height of image
