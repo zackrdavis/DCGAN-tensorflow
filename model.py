132bcee31aa23c1a2c33a981bcc262d1bc2349a5
zack <zackrdavis@gmail.com>
2016-12-18 14:48:04 -0500

    output samples right away

--- model.py
+++ model.py
@@ -226,12 +226,13 @@ class DCGAN(object):
                     errD_real = self.d_loss_real.eval({self.images: batch_images})
                     errG = self.g_loss.eval({self.z: batch_z})
 
+
                 counter += 1
                 print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
                     % (epoch, idx, batch_idxs,
                         time.time() - start_time, errD_fake+errD_real, errG))
 
-                if np.mod(counter, 100) == 1:
+                if np.mod(counter, 100) == 1 || counter == 1:
                     if config.dataset == 'mnist':
                         samples, d_loss, g_loss = self.sess.run(
                             [self.sampler, self.d_loss, self.g_loss],
