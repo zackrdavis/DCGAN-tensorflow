0af963dd05e237372bb0c9d26e738e25437501e8
zack <zackrdavis@gmail.com>
2016-12-18 14:50:27 -0500

    proper conditional, derp

--- model.py
+++ model.py
@@ -232,7 +232,7 @@ class DCGAN(object):
                     % (epoch, idx, batch_idxs,
                         time.time() - start_time, errD_fake+errD_real, errG))
 
-                if np.mod(counter, 100) == 1 || counter == 1:
+                if np.mod(counter, 100) == 1 or counter == 1:
                     if config.dataset == 'mnist':
                         samples, d_loss, g_loss = self.sess.run(
                             [self.sampler, self.d_loss, self.g_loss],
