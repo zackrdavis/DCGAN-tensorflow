52117a59f9bde7f02359e4a594fa3e505f05d050
zack <zackrdavis@gmail.com>
2017-01-05 02:48:46 -0500

    fix resize conv

--- ops.py
+++ ops.py
@@ -72,34 +72,29 @@ def conv2d(input_, output_dim,
         conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
 
         return conv
-
+        
 def deconv2d(input_, output_shape,
              k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
              name="deconv2d", with_w=False):
+
     with tf.variable_scope(name):
         # filter : [height, width, output_channels, in_channels]
-        w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
+        w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_shape[-1]],
                             initializer=tf.random_normal_initializer(stddev=stddev))
                             
         up = tf.image.resize_nearest_neighbor(input_, [output_shape[1], output_shape[2]])
         
-        padded = tf.pad(up, [[0,0], [2,2], [2,2], [0,0]], mode='SYMMETRIC')
-        
-        try:
-            deconv = tf.nn.conv2d(padded, w, strides=[1, 1, 1, 1], padding='VALID')
+        padded = tf.pad(up, [[0,0], [2,2], [2,2], [0,0]], mode="SYMMETRIC")
 
-        # Support for verisons of TensorFlow before 0.7.0
-        except AttributeError:
-            deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape,
-                                strides=[1, d_h, d_w, 1])
+        conv = tf.nn.conv2d(padded, w, strides=[1, 1, 1, 1], padding='VALID')
 
         biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
-        deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+        conv = tf.reshape(tf.nn.bias_add(conv, biases), output_shape)
 
         if with_w:
-            return deconv, w, biases
+            return conv, w, biases
         else:
-            return deconv
+            return conv
        
 
 def lrelu(x, leak=0.2, name="lrelu"):
